320484403718f35eb2fd7186826cb645ab43f22b
abogailo <abagailanne@gmail.com>
2019-11-01 20:09:18 -0400

    real mashed together but login works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,32 @@
 class SessionsController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        if auth_hash
+            user = User.find_or_create_by_omniauth(auth_hash)
+            session[:user_id] = user.id
+            redirect_to root_path
+        else
+            @user = User.find_by(email: params[:email])
+            if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to root_path
+            else
+                render :new
+            end
+        end
+    end
+
+    def destroy
+        session.delete :user_id
+        redirect_to new_session_path
+    end
+
+    private
+
+    def auth_hash
+        request.env['omniauth.auth']
+    end
 end
